95ab5cac89be85419ff1ead13cfacfc761c94478
Ana Nogal <ana.nogal@gmail.com>
2014-08-17 08:33:21 +0100

    Added some comments

--- directory.rb
+++ directory.rb
@@ -1,21 +1,23 @@
-puts("The students of my cohort at Makers Academy")
-puts("---------------------")
-puts("Enrique Comba Riepenhausen")
-puts("Stephen Lloyd")
-puts("Alex Peattie")
-puts("vic329")
-puts("NicolePell")
-puts("Bernard")
-puts("elenagarrone")
-puts("Yvette Cook")
-puts("Stephen Giles")
-puts("Ana Isabel Nogal")
-puts("craigh44")
-puts("Tim Scully")
-puts("Ella Schofield")
-puts("Fadie H")
-puts("mala23")
-
+#first we print the list of students
+puts "The students of my cohort at Makers Academy"
+puts "---------------------"
+puts "Enrique Comba Riepenhausen"
+puts "Stephen Lloyd"
+puts "Alex Peattie"
+puts "vic329"
+puts "NicolePell"
+puts "Bernard"
+puts "elenagarrone"
+puts "Yvette Cook"
+puts "Stephen Giles"
+puts "Ana Isabel Nogal"
+puts "craigh44"
+puts "Tim Scully"
+puts "Ella Schofield"
+puts "Fadie H"
+puts "mala23"
+#finally, we print the total
 print "Overall, we have "
+#it's important that print() 
 print 15
-puts(" great students")
+puts " great students"
