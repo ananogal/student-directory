92590510c5f03534a02172f967a7e88efba4ff00
Ana Nogal <ana.nogal@gmail.com>
2014-08-17 08:24:33 +0100

    Added student count

---
+++ directory.rb
@@ -0,0 +1,21 @@
+puts("The students of my cohort at Makers Academy")
+puts("---------------------")
+puts("Enrique Comba Riepenhausen")
+puts("Stephen Lloyd")
+puts("Alex Peattie")
+puts("vic329")
+puts("NicolePell")
+puts("Bernard")
+puts("elenagarrone")
+puts("Yvette Cook")
+puts("Stephen Giles")
+puts("Ana Isabel Nogal")
+puts("craigh44")
+puts("Tim Scully")
+puts("Ella Schofield")
+puts("Fadie H")
+puts("mala23")
+
+print "Overall, we have "
+print 15
+puts(" great students")
